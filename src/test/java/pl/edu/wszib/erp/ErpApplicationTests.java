0a3078fe6a4b81a0f74352f36e5c7ccaee75dd59
Ewa Słota <ewa4852@gmail.com>
2024-06-03 23:26:55 +0200

    Initial commit - spring boot with rest, mysql, actuator and swagger.

---
+++ src/test/java/pl/edu/wszib/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
